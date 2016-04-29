f5f537db9fe1ea2b79db33ba72aaab7203c8a5b5
ilaryionava <iratweety@gmail.com>
2016-04-29 07:07:05 -0700

    - fix for publishing

--- setup.py
+++ setup.py
@@ -2,16 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
         name='iofabric',
-
-        version='1.0.0',
-
+        packages = ["iofabric"],
+        version='1.0.2',
         description='Native python SDK for ioTracks development.',
-
         url='http://iotracks.com/',
-
         author='IOTRACKS',
         author_email='kilton@iotracks.com',
-
         keywords='iotracks, IoT',
-
 )
