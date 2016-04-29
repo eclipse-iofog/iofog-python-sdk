f5b00d18542418a90d0dd76501ee84d416edd960
ilaryionava <iratweety@gmail.com>
2016-04-29 05:37:45 -0700

    - setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='iofabric',
+
+        version='1.0.0',
+
+        description='Native python SDK for ioTracks development.',
+
+        url='http://iotracks.com/',
+
+        author='IOTRACKS',
+        author_email='kilton@iotracks.com',
+
+        keywords='iotracks, IoT',
+
+)
