4a10be40c3881c1199735c794dc38ad4b3398a4a
pavelkazlou <pkazlou@4tegroup.by>
2016-05-11 16:11:28 +0300

    Update client.py
    Fix ACK message logic.

--- iofabric/client.py
+++ iofabric/client.py
@@ -1,4 +1,3 @@
-
 from ws4py.client.threadedclient import WebSocketClient
 import threading
 import iomessage
@@ -27,21 +26,23 @@ class Client(WebSocketClient):
 
 
     def received_message(self, m):
-        msg_data=bytearray(m.data)
+        msg_data=bytearray(m.data[5:])
+        #msg_data=m.data
         if len(msg_data) == 0:
             return
-        opt_code=msg_data[0]
+        opt_code=bytearray(m.data)[0]
         if opt_code == NEW_MESSAGE:
-            msg=iomessage.bytes2message(msg_data[1:])
-            self.send(bytearray([RECEIPT]))
+            msg=iomessage.bytes2message(msg_data)
+            self.send(bytearray([ACK]), binary=True)
             self.listener.onMessage(msg)
-        if opt_code == RECEIPT:
-            self.send(bytearray([ACK]))
         if opt_code == ACK:
             print "ACK recieved."
+        if opt_code == RECEIPT:
+            self.send(bytearray([ACK]), binary=True)
         if opt_code == CONTROL:
             req = urllib2.Request("http://" + get_host() + ":54321/v2/config/get", "{\"id\":\"" + self.container_id + "\"}", {'Content-Type': 'application/json'})
             response = urllib2.urlopen(req)
+            self.send(bytearray([ACK]), binary=True)
             self.listener.onUpdateConfig(json.loads(response.read()))
 
     def send_message(self, msg):
