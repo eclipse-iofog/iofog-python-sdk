258e4f152ef40298ecf949b1e46360bff454162e
sashayakovtseva <sashayakovtseva@gmail.com>
2017-04-05 13:11:59 +0300

    pypi version updated in setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='iofog-container-sdk',
-    version='1.0.4',
+    version='1.0.5',
     packages=['iofog_container_sdk'],
     url='http://iotracks.com/',
     license='',
