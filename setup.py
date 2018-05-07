c0267edac4ebc6cea03b2ff771dd91c27e092e4d
ilaryionava <ilaryionava@gmail.com>
2018-05-07 18:52:46 +0300

    udated meta-info for new account and name

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 from distutils.core import setup
 
 setup(
-    name='iofog-container-sdk',
-    version='1.0.5',
-    packages=['iofog_container_sdk'],
-    url='http://iofog.org',
+    name='iofog-python-sdk',
+    version='0.0.1',
+    project_urls={
+        'Documentation': 'https://github.com/ioFog/iofog-python-sdk/blob/master/README.md',
+        'Source': 'https://github.com/ioFog/iofog-python-sdk.git',
+        'Tracker': 'https://github.com/ioFog/iofog-python-sdk/issues',
+        'Eclipse ioFog': 'http://iofog.org'
+    },
+    packages=['iofog_python_sdk'],
+    url='https://github.com/ioFog/iofog-python-sdk',
     license='EPL-2.0',
     author='Eclipse ioFog',
     author_email='edgemaster@iofog.org',
     description='Native python SDK for Eclipse ioFog development.',
     requires=['ws4py'],
-    keywords='iofog, IoT, Eclipse, fog, edgeworx',
+    keywords='iofog IoT Eclipse fog computing edgeworx',
 )
