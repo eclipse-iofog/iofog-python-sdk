a8211ff60a497a95b9ef4371cd0915c3ac3791f7
Railag <whenthegroundcavedin@gmail.com>
2018-12-13 15:04:50 +0300

    version 0.0.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from distutils.core import setup
 
 setup(
     name='iofog-python-sdk',
-    version='0.0.1',
+    version='0.0.2',
     project_urls={
         'Documentation': 'https://github.com/ioFog/iofog-python-sdk/blob/master/README.md',
         'Source': 'https://github.com/ioFog/iofog-python-sdk.git',
