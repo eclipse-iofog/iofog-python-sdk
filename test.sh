a34d1ba3d15efc249fe7499a21ca50b871cd9c7d
Saeid Rezaei Baghbidi <saeid@edgeworx.io>
2019-11-07 20:21:40 -0800

    Test

--- test.sh
+++ test.sh
@@ -1,31 +1,18 @@
 #!/usr/bin/env bash
 
-PYTHON_VERSIONS=( "python3" "python2" )
-FAILURES=( 0 0 )
+PYTHON_VERSION=$1
 
-idx=0
-for version in ${PYTHON_VERSIONS[@]}; do
+docker run --name send -d iofog/test-python-sdk-send:$PYTHON_VERSION 1>/dev/null
+docker run --name recieve -d iofog/test-python-sdk-recieve:$PYTHON_VERSION 1>/dev/null
 
-    docker run --name send -d iofog/test-python-sdk-send:$version 1>/dev/null
-    docker run --name recieve -d iofog/test-python-sdk-recieve:$version 1>/dev/null
+SEND_CONTAINER_ID=$(docker ps -aqf "name=send")
+RECIEVE_CONTAINER_ID=$(docker ps -aqf "name=recieve")
 
-    SEND_CONTAINER_ID=$(docker ps -aqf "name=send")
-    RECIEVE_CONTAINER_ID=$(docker ps -aqf "name=recieve")
+if [[ -z $RECIEVE_CONTAINER_ID || -z $SEND_CONTAINER_ID ]]; then
+    exit 1
+fi
 
-    if [[ -z $RECIEVE_CONTAINER_ID || -z $SEND_CONTAINER_ID ]]; then
-        echo "${version} has failed to send/recieve data"
-        ${FAILURES[idx]} = 1
-    fi
-    idx+=1
-
-    docker rm -f send 1>/dev/null
-    docker rm -f recieve 1>/dev/null
-done
-
-for failure in ${FAILURES[@]}; do
-    if [[ ${failure} == 1 ]]; then
-        exit 1
-    fi
-done
+docker rm -f send 1>/dev/null
+docker rm -f recieve 1>/dev/null
 
 exit 0
