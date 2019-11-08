d327a42be5a6c57e3d84f5222cf26f3188953061
Saeid Rezaei Baghbidi <saeid@edgeworx.io>
2019-11-07 20:17:53 -0800

    Test

--- test.sh
+++ test.sh
@@ -6,20 +6,20 @@ FAILURES=( 0 0 )
 idx=0
 for version in ${PYTHON_VERSIONS[@]}; do
 
-    docker pull iofog/test-python-sdk-send::${version}
-    docker pull iofog/test-python-sdk-recieve:${version}
+    docker run --name send -d iofog/test-python-sdk-send:$version 1>/dev/null
+    docker run --name recieve -d iofog/test-python-sdk-recieve:$version 1>/dev/null
 
-    docker run -d iofog/test-python-sdk-send:${version}
-    docker run -d iofog/test-python-sdk-recieve:${version}
+    SEND_CONTAINER_ID=$(docker ps -aqf "name=send")
+    RECIEVE_CONTAINER_ID=$(docker ps -aqf "name=recieve")
 
-    RECIEVE_CONTAINER_ID=$( docker ps | grep "python recieve.py" | awk '{print $1}' )
-    SEND_CONTAINER_ID=$( docker ps | grep "python send.py" | awk '{print $1}' )
-
-    if [[ "${RECIEVE_CONTAINER_ID}" -eq "" || "${SEND_CONTAINER_ID}" -eq "" ]]; then
+    if [[ -z $RECIEVE_CONTAINER_ID || -z $SEND_CONTAINER_ID ]]; then
         echo "${version} has failed to send/recieve data"
-        ${FAILURES[idx]}= 1
+        ${FAILURES[idx]} = 1
     fi
     idx+=1
+
+    docker rm -f send 1>/dev/null
+    docker rm -f recieve 1>/dev/null
 done
 
 for failure in ${FAILURES[@]}; do
