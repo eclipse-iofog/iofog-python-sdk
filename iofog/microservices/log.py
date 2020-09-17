c633adca606b1ef08bc706692c96df76573b28b6
SergeRadinovich <serge@edgeworx.io>
2020-09-18 09:59:06 +1200

    Add microservices.log

---
+++ iofog/microservices/log.py
@@ -0,0 +1,58 @@
+#********************************************************************************
+#  Copyright (c) 2018 Edgeworx, Inc.
+#
+#  This program and the accompanying materials are made available under the
+#  terms of the Eclipse Public License v. 2.0 which is available at
+#  http://www.eclipse.org/legal/epl-2.0
+#
+#  SPDX-License-Identifier: EPL-2.0
+#********************************************************************************
+
+import json_logging, logging, sys
+import datetime
+import traceback
+import json
+
+class CustomJSONLog(logging.Formatter):
+    """
+    Customized logger
+    """
+
+    def format(self, record):
+        json_log_object = {"timestamp": datetime.datetime.utcnow().isoformat(),
+                           "level": record.levelname,
+                           "message": record.getMessage(),
+                           }
+        return json.dumps(json_log_object)
+
+json_logging.init_non_web(custom_formatter=CustomJSONLog, enable_json=True)
+
+class Logger():
+    """
+    ioFog Logging Client
+    """
+
+    def __init__(self, name, handlers = [logging.StreamHandler(sys.stdout)]):
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(logging.DEBUG)
+        for handler in handlers:
+            self.logger.addHandler(handler)
+        self.extra = {} #{'props' : {'extra_property' : 'extra_value'}}
+    
+    def info(self, msg):
+        self.logger.info(msg, extra = self.extra)
+
+    def debug(self, msg):
+        self.logger.debug(msg, extra = self.extra)
+
+    def warn(self, msg):
+        self.logger.warn(msg, extra = self.extra)
+
+    def error(self, msg):
+        self.logger.error(msg, extra = self.extra)
+
+#if __name__ == "__main__":
+#    log = Logger("serge")
+#    log.info("hi")
+#    log2 = Logger("serge2", [logging.FileHandler("/tmp/log.txt")])
+#    log2.debug("hi")
