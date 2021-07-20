9da23279a947e7c260250cfc13162f7ed2072d9b
sergerad <serge.radinovich@gmail.com>
2021-07-20 16:08:47 +1200

    v3.0.0-alpha4

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='iofog',
-    version='3.0.0-alpha3',
+    version='3.0.0-alpha4',
     project_urls={
         'Documentation': 'https://github.com/eclipse-iofog/iofog-python-sdk/blob/master/README.md',
         'Source': 'https://github.com/eclipse-iofog/iofog-python-sdk.git',
